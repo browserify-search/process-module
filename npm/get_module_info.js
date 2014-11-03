4332315eee1786929a82d4aadd70d04d594afee5
Toby Ho <airportyh@gmail.com>
2014-11-03 15:14:45 -0500

    First commit. Extracted from browserify-search/scripts.

---
+++ npm/get_module_info.js
@@ -0,0 +1,16 @@
+var assert = require('assert')
+var is = require('is-type')
+var request = require('superagent')
+
+module.exports = function(module, callback){
+  assert(is.string(module))
+  assert(is.function(callback))
+  
+  request.get('http://registry.npmjs.org/' + module + '/')
+    .end(function(err, resp){
+      err = err || resp.error
+      if (err) return callback(err)
+      var body = JSON.parse(resp.text)
+      callback(null, body)
+    })
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+var processModule = require('./index')
+
+processModule('ispy', function(err, results){
+  if (err) return console.error(err.message)
+
+  console.log(results)
+})
